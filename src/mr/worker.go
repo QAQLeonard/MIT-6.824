b26088eddf6537bad5376e23b59cf7b37a922f78
Leonard <leonardzhou2077@gmail.com>
2023-09-20 20:17:41 +0800

    update

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -13,10 +13,22 @@ import (
 	"time"
 )
 
+//
 // Map functions return a slice of KeyValue.
+//
 type KeyValue struct {
 	Key   string
 	Value string
+}
+
+//
+// use ihash(key) % NReduce to choose the reduce
+// task number for each KeyValue emitted by Map.
+//
+func ihash(key string) int {
+	h := fnv.New32a()
+	h.Write([]byte(key))
+	return int(h.Sum32() & 0x7fffffff)
 }
 
 // for sorting by key.
@@ -27,66 +39,34 @@ func (a ByKey) Len() int           { return len(a) }
 func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
 
-// use ihash(key) % NReduce to choose the reduce
-// task number for each KeyValue emitted by Map.
-func ihash(key string) int {
-	h := fnv.New32a()
-	h.Write([]byte(key))
-	return int(h.Sum32() & 0x7fffffff)
-}
-
+//
 // main/mrworker.go calls this function.
+//
 func Worker(mapf func(string, string) []KeyValue, reducef func(string, []string) string) {
-
+	// 启动worker
 	for {
+		// worker从master获取任务
 		task := getTask()
-		if task.TaskState == Map {
-			// mapper(&task, mapf)
-		} else if task.TaskState == Reduce {
-			// reducer(&task, reducef)
-		} else if task.TaskState == Wait {
-			time.Sleep(3 * time.Second)
-		} else if task.TaskState == Exit {
+
+		// 拿到task之后，根据task的state，map task交给mapper， reduce task交给reducer
+		// 额外加两个state，让 worker 等待 或者 直接退出
+		switch task.TaskState {
+		case Map:
+			mapper(&task, mapf)
+		case Reduce:
+			reducer(&task, reducef)
+		case Wait:
+			time.Sleep(5 * time.Second)
+		case Exit:
 			return
 		}
 	}
 }
 
-func mapper(task *Task, mapf func(string, string) []KeyValue) {
-	//从文件名读取content
-	content, err := ioutil.ReadFile(task.Input)
-	if err != nil {
-		log.Fatal("Failed to read file: "+task.Input, err)
-	}
-	//将content交给mapf，缓存中间结果
-	intermediates := mapf(task.Input, string(content))
-
-	//缓存后的结果会写到本地磁盘，并切成R份
-	//切分方式是根据key做hash
-
-	//切割
-	buffer := make([][]KeyValue, task.NReducer)
-	for _, intermediate := range intermediates {
-		slot := ihash(intermediate.Key) % task.NReducer
-		buffer[slot] = append(buffer[slot], intermediate)
-	}
-
-	//写
-	mapOutput := make([]string, 0)
-	for i := 0; i < task.NReducer; i++ {
-		mapOutput = append(mapOutput, writeFile(task.TaskNumber, i, &buffer[i]))
-	}
-
-	task.Intermediates = mapOutput
-	//通知master，map task完成
-	TaskCompleted(task)
-}
-
 func reducer(task *Task, reducef func(string, []string) string) {
-	//先读取keyvalue
-	intermediate := *readFile(task.Intermediates)
-
-	//按照keyvalue排序
+	//先从filepath读取intermediate的KeyValue
+	intermediate := *readFromLocalFile(task.Intermediates)
+	//根据kv排序
 	sort.Sort(ByKey(intermediate))
 
 	dir, _ := os.Getwd()
@@ -94,9 +74,10 @@ func reducer(task *Task, reducef func(string, []string) string) {
 	if err != nil {
 		log.Fatal("Failed to create temp file", err)
 	}
-	//进行整合累加分组合并，reduce
+	// 这部分代码修改自mrsequential.go
 	i := 0
 	for i < len(intermediate) {
+		//将相同的key放在一起分组合并
 		j := i + 1
 		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
 			j++
@@ -105,71 +86,59 @@ func reducer(task *Task, reducef func(string, []string) string) {
 		for k := i; k < j; k++ {
 			values = append(values, intermediate[k].Value)
 		}
-		//get result from reducef
+		//交给reducef，拿到结果
 		output := reducef(intermediate[i].Key, values)
-		//write in
+		//写到对应的output文件
 		fmt.Fprintf(tempFile, "%v %v\n", intermediate[i].Key, output)
 		i = j
 	}
-
 	tempFile.Close()
-	
 	oname := fmt.Sprintf("mr-out-%d", task.TaskNumber)
 	os.Rename(tempFile.Name(), oname)
 	task.Output = oname
 	TaskCompleted(task)
 }
 
-// send an RPC request to the master, wait for the response.
-// usually returns true.
-// returns false if something goes wrong.
-func call(rpcname string, args interface{}, reply interface{}) bool {
-	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
-	sockname := masterSock()
-	c, err := rpc.DialHTTP("unix", sockname)
+func mapper(task *Task, mapf func(string, string) []KeyValue) {
+	//从文件名读取content
+	content, err := ioutil.ReadFile(task.Input)
 	if err != nil {
-		log.Fatal("dialing:", err)
+		log.Fatal("Failed to read file: "+task.Input, err)
 	}
-	defer c.Close()
+	//将content交给mapf，缓存结果
+	intermediates := mapf(task.Input, string(content))
 
-	err = c.Call(rpcname, args, reply)
-	if err == nil {
-		return true
+	//缓存后的结果会写到本地磁盘，并切成R份
+	//切分方式是根据key做hash
+	buffer := make([][]KeyValue, task.NReducer)
+	for _, intermediate := range intermediates {
+		slot := ihash(intermediate.Key) % task.NReducer
+		buffer[slot] = append(buffer[slot], intermediate)
 	}
-
-	fmt.Println(err)
-	return false
+	mapOutput := make([]string, 0)
+	for i := 0; i < task.NReducer; i++ {
+		mapOutput = append(mapOutput, writeToLocalFile(task.TaskNumber, i, &buffer[i]))
+	}
+	//R个文件的位置发送给master
+	task.Intermediates = mapOutput
+	TaskCompleted(task)
 }
 
 func getTask() Task {
+	// worker从master获取任务
 	args := ExampleArgs{}
 	reply := Task{}
 	call("Master.AssignTask", &args, &reply)
 	return reply
 }
 
-// example function to show how to make an RPC call to the master.
-//
-// the RPC argument and reply types are defined in rpc.go.
-func CallExample() {
-
-	// declare an argument structure.
-	args := ExampleArgs{}
-
-	// fill in the argument(s).
-	args.X = 99
-
-	// declare a reply structure.
+func TaskCompleted(task *Task) {
+	//通过RPC，把task信息发给master
 	reply := ExampleReply{}
-
-	// send the RPC request, wait for the reply.
-	call("Master.Example", &args, &reply)
-
-	// reply.Y should be 100.
-	fmt.Printf("reply.Y %v\n", reply.Y)
+	call("Master.TaskCompleted", task, &reply)
 }
 
-func writeFile(x int, y int, kvs *[]KeyValue) string {
+func writeToLocalFile(x int, y int, kvs *[]KeyValue) string {
 	dir, _ := os.Getwd()
 	tempFile, err := ioutil.TempFile(dir, "mr-tmp-*")
 	if err != nil {
@@ -187,7 +156,7 @@ func writeFile(x int, y int, kvs *[]KeyValue) string {
 	return filepath.Join(dir, outputName)
 }
 
-func readFile(files []string) *[]KeyValue {
+func readFromLocalFile(files []string) *[]KeyValue {
 	kva := []KeyValue{}
 	for _, filepath := range files {
 		file, err := os.Open(filepath)
@@ -207,8 +176,26 @@ func readFile(files []string) *[]KeyValue {
 	return &kva
 }
 
-func TaskCompleted(task *Task) {
-	//通过RPC，把task信息发给master
-	reply := ExampleReply{}
-	call("Master.TaskCompleted", task, &reply)
+//
+// send an RPC request to the master, wait for the response.
+// usually returns true.
+// returns false if something goes wrong.
+//
+func call(rpcname string, args interface{}, reply interface{}) bool {
+	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+	sockname := masterSock()
+	c, err := rpc.DialHTTP("unix", sockname)
+	if err != nil {
+		// Master结束进程，退出worker
+		os.Exit(0)
+	}
+	defer c.Close()
+
+	err = c.Call(rpcname, args, reply)
+	if err == nil {
+		return true
+	}
+
+	fmt.Println(err)
+	return false
 }
